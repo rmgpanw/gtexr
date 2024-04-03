c85ef696ef4cb37de10474db51a393af3f0cfbdb
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-03 15:02:51 +0100

    add get_genes()

---
+++ R/get_genes.R
@@ -0,0 +1,24 @@
+#' [Get Genes](https://gtexportal.org/api/v2/redoc#tag/Reference-Genome-Endpoints/operation/get_genes_api_v2_reference_gene_get)
+#'
+#' This service returns information about reference genes. A genome build and
+#' GENCODE version must be provided.
+#'
+#' - Genes are searchable by gene symbol, GENCODE ID and versioned GENCODE ID.
+#' - Versioned GENCODE ID is recommended to ensure unique ID matching.
+#' - By default, this service queries the genome build and GENCODE version used by the latest GTEx release.
+#'
+#' @inheritParams get_gene_search
+#'
+#' @return A tibble
+#' @export
+#' @family Reference Genome Endpoints
+#'
+#' @examples
+#' get_genes("CRP")
+get_genes <- function(geneId,
+                      gencodeVersion = "v26",
+                      genomeBuild = "GRCh38/hg38",
+                      page = 0,
+                      itemsPerPage = 250) {
+  gtex_query(endpoint = "reference/gene")
+}
