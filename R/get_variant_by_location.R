2ed6203d5818b48e444f88a43bc7829e3977e28d
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-03 13:00:27 +0100

    add get_variant_by_location()

---
+++ R/get_variant_by_location.R
@@ -0,0 +1,29 @@
+#' Get Variant By Location
+#'
+#' [This service allows the user to query information about variants on a
+#' certain chromosome at a certain
+#' location.](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_variant_by_location_api_v2_dataset_variantByLocation_get)
+#'
+#' @param start Integer.
+#' @param end Integer.
+#' @param chromosome String.
+#' @param sortDirection String (default = "asc"). Either "asc" or "desc".
+#' @param page Integer.
+#' @param itemsPerPage Integer.
+#'
+#' @return A tibble.
+#' @export
+#' @family Datasets Endpoints
+#'
+#' @examples
+#' get_variant_by_location(start = 153209600,
+#'                         end = 153209700,
+#'                         chromosome = "chr1")
+get_variant_by_location <- function(start,
+                                    end,
+                                    chromosome,
+                                    sortDirection = "asc",
+                                    page = 0,
+                                    itemsPerPage = 250) {
+  gtex_query(endpoint = "dataset/variantByLocation")
+}
