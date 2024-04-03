e8f26c95c04b07006cfc2124ac0842accb251a0d
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-03 13:40:59 +0100

    update get_service_info() to return a single row tibble; move tidyr to Imports in DESCRIPTION

--- R/get_service_info.R
+++ R/get_service_info.R
@@ -9,5 +9,8 @@
 #' @examples
 #' get_service_info()
 get_service_info <- function() {
-  gtex_query(endpoint = NULL)
+  gtex_query(endpoint = NULL, return_raw = TRUE) |>
+    purrr::map_at("organization", tibble::as_tibble) |>
+    tibble::as_tibble() |>
+    tidyr::unnest(cols = "organization", names_sep = "_")
 }
