0f674403842a937eb13428b723b81215a32c10bd
Benjamin Zuckerman <benjamin.zuckerman@kcl.ac.uk>
2024-04-10 12:12:50 +0100

    get_sqtl_genes

---
+++ R/get_sqtl_genes.R
@@ -0,0 +1,6 @@
+get_sqtl_genes <- function(tissueSiteDetailID,
+                           datasetID = "gtex_v8",
+                           page = 0,
+                           itemsPerPage = 250){
+  gtex_query(endpoint = "association/sgene")
+}
