3ae3ec30c36c3c30e290f6212906162b3f9dc1c4
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-14 22:27:16 +0100

    update get_significant_single_tissue_eqtls_by_location() - should now return a usable tibble (also fix typo in documentation example)

--- R/get_significant_single_tissue_eqtls_by_location.R
+++ R/get_significant_single_tissue_eqtls_by_location.R
@@ -21,17 +21,20 @@
 #'
 #' @examples
 #' \dontrun{
-#' get_significant_single_tissue_eqtls_by_location(tissueSiteDetailId = "Artery_Aorta",
-#' start = 10000,
-#' end = 250000,
-#' chromosome = "chr_11")
+#' get_significant_single_tissue_eqtls_by_location(
+#'   tissueSiteDetailId = "Artery_Aorta",
+#'   start = 10000,
+#'   end = 250000,
+#'   chromosome = "chr11")
 #' }
 get_significant_single_tissue_eqtls_by_location <- function(tissueSiteDetailId,
                                                             start,
                                                             end,
                                                             chromosome,
                                                             datasetId = "gtex_v8") {
-  gtex_query(endpoint = "association/singleTissueEqtlByLocation")
+  gtex_query(endpoint = "association/singleTissueEqtlByLocation",
+             return_raw = TRUE)$singleTissueEqtl |>
+    purrr::map(tibble::as_tibble) |> dplyr::bind_rows()
 }
 
 
