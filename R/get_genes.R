31258e5ad0cac228205acbcc7ea97977cc8e4cf7
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-21 22:50:47 +0100

    fix typo in get_genes() link to GTEx API site

--- R/get_genes.R
+++ R/get_genes.R
@@ -7,7 +7,7 @@
 #' - Versioned GENCODE ID is recommended to ensure unique ID matching.
 #' - By default, this service queries the genome build and GENCODE version used by the latest GTEx release.
 #'
-#' [GTEx API Portal docs]((https://gtexportal.org/api/v2/redoc#tag/Reference-Genome-Endpoints/operation/get_genes_api_v2_reference_gene_get))
+#' [GTEx API Portal docs](https://gtexportal.org/api/v2/redoc#tag/Reference-Genome-Endpoints/operation/get_genes_api_v2_reference_gene_get)
 #'
 #' @inheritParams gtexr_arguments
 #'
