3ce3208cfb8366bf37d170202bc77806a91ba8eb
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-26 01:32:31 +0100

    update get_clustered_median_transcript_expression()

--- R/get_clustered_median_transcript_expression.R
+++ R/get_clustered_median_transcript_expression.R
@@ -1,11 +1,11 @@
-#' Get Clustered Median Transcript Expression
+#'Get Clustered Median Transcript Expression
 #'
-#' @description
+#'@description
 #' - Find median transcript expression data of all known transcripts of a gene along with hierarchical clusters .
 #' - Returns median normalized expression in tissues of all known transcripts of a given gene along with the hierarchical clustering results of tissues and genes, based on expression, in Newick format.
 #' - Results may be filtered by dataset, gene or tissue, but at least one gene must be provided.
 #' - The hierarchical clustering is performed by calculating Euclidean distances and using the average linkage method.
-#' - This endpoint is not paginated.
+#' - **This endpoint is not paginated.**
 #' - By default, this service queries the latest GTEx release.
 #'
 #'[GTEx Portal API
@@ -13,16 +13,26 @@
 #'
 #'@inheritParams gtexr_arguments
 #'
-#' @return A Tibble
-#' @export
-#' @family Expression Data Endpoints
+#'@return A list with items "clusters" (a list) and "medianTranscriptExpression"
+#'  (a tibble).
+#'@export
+#'@family Expression Data Endpoints
 #'
 #' @examples
 #' \dontrun{
-#' get_clustered_median_transcript_expression(gencodeIds = "ENSG00000132693.12")
+#' get_clustered_median_transcript_expression(gencodeIds = c("ENSG00000203782.5",
+#'                                                           "ENSG00000132693.12"))
 #' }
 get_clustered_median_transcript_expression <- function(gencodeIds,
                                                        datasetId = "gtex_v8",
                                                        tissueSiteDetailIds = NULL){
-  gtex_query(endpoint = "expression/clusteredMedianTranscriptExpression")
+  response <- gtex_query(endpoint = "expression/clusteredMedianTranscriptExpression",
+             return_raw = TRUE)
+
+  response$medianTranscriptExpression <-
+    response$medianTranscriptExpression |>
+    purrr::map(tibble::as_tibble) |>
+    dplyr::bind_rows()
+
+  return(response)
 }
