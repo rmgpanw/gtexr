05dbb80262548952f37da8bac974a6b014b836ec
rmgpanw <rmgpanw@ucl.ac.uk>
2024-04-26 01:10:41 +0100

    update get_single_nucleus_gex() to work when `excludeDataArray = FALSE`

--- R/get_single_nucleus_gex.R
+++ R/get_single_nucleus_gex.R
@@ -21,22 +21,36 @@ get_single_nucleus_gex <- function(gencodeIds,
                                    tissueSiteDetailIds = NULL,
                                    excludeDataArray = TRUE,
                                    page = 0,
-                                   itemsPerPage = 250){
+                                   itemsPerPage = 250) {
   gtex_query(endpoint = "expression/singleNucleusGeneExpression",
              return_raw = TRUE)$data |>
-    purrr::map(\(x) x |>
-                 purrr::imap(\(x, idx) ifelse(
-                   is.list(x),
-                   tibble::tibble(
-                     data = purrr::map_depth(
-                       x,
-                       purrr::pluck_depth(x) - 2,
-                       \(x) tibble::as_tibble(purrr::compact(x))
-                     ) |>
-                       dplyr::bind_rows()
-                   ),
-                   x
-                 )) |>
-                 tibble::as_tibble()) |>
+
+    # returns a list of lists, with one list for each tissue type. Aim: return a
+    # single row tibble for each tissue type
+    purrr::map(\(x) {
+      result <- x |>
+
+        # for each tissue type, convert "cellTypes" item to a tibble
+        purrr::map_if(is.list,
+                      # "cellTypes" column is a list of lists, one list
+                      # for each cell type
+                      \(x) purrr::map(x, \(x) {
+
+                        # process 'data', if `excludeDataArray` is `FALSE`
+                        data_values <- unlist(x$data)
+
+                        if (!is.null(data_values)) {
+                          x$data <- list(data_values)
+                        }
+
+                        x |>
+                          purrr::compact() |>
+                          tibble::as_tibble()
+                      }))
+
+      x$cellTypes <- list(dplyr::bind_rows(result$cellTypes))
+
+      tibble::as_tibble(x)
+    }) |>
     dplyr::bind_rows()
 }
