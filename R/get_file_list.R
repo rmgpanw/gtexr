5443adebdd376a80dc9554302c6b67d6fed28af5
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-15 23:35:32 +0100

    update get_file_list() - now outputs tibble with named nested lists in column 'filesets'

--- R/get_file_list.R
+++ R/get_file_list.R
@@ -1,21 +1,34 @@
 #' Get File List
 #'
 #' @description
-#' - Get all the files in GTEx dataset for Download page
+#' Get all the files in GTEx dataset for Download page
 #'
 #' [GTEx Portal API
 #' documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_file_list_api_v2_dataset_fileList_get)
 #'
-#'
-#' @return A Tibble
+#' @return A tibble
 #' @export
 #' @family Datasets Endpoints
 #'
 #' @examples
 #' get_file_list()
 get_file_list <- function() {
-  gtex_query(endpoint = "dataset/fileList",
-             return_raw = TRUE) |>
-    purrr::map(tibble::as_tibble) |>
-    dplyr::bind_rows()
+  result <- gtex_query(endpoint = "dataset/fileList", return_raw = TRUE)
+  result |>
+    purrr::map(\(x) x |>
+                 purrr::map_at("filesets", list) |>
+                 tibble::as_tibble(),
+               .progress = TRUE) |>
+    dplyr::bind_rows() |>
+    # add names to lists ('filesets' and 'files')
+    dplyr::mutate("filesets" = purrr::map(.data[["filesets"]], \(x) {
+      names(x) <- purrr::map_chr(x, \(y) y$name)
+      x <- x |>
+        purrr::map(\(x) x |>
+                     purrr::map_at("files", \(y) {
+                       names(y) <- purrr::map_chr(y, \(z) z$name)
+                       y
+                     }))
+      x
+    }))
 }
