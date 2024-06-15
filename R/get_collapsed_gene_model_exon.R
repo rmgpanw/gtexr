112ca2cf3304f895eda381f2bb6c5f03269cdd8d
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-15 23:03:36 +0100

    update get_collapsed_gene_model_exon()

--- R/get_collapsed_gene_model_exon.R
+++ R/get_collapsed_gene_model_exon.R
@@ -1,25 +1,26 @@
-#'Get Collapsed Gene Model Exon
+#' Get Collapsed Gene Model Exon
 #'
-#'@description
+#' @description This service returns the collapsed exons in the gene model of
+#' the given gene. Gene-level and exon-level expression quantification were
+#' based on the GENCODE annotation, collapsed to a single transcript model for
+#' each gene using an algorithm developed by the GTEx analysis team.
 #'
-#' - This service returns the collapsed exons in the gene model of the given gene.
-#' - Gene-level and exon-level expression quantification were based on the GENCODE
-#'annotation, collapsed to a single transcript model for each gene using an
-#'algorithm developed by the GTEx analysis team.
-#' - By default, this service queries the models used by the latest GTEx release.
+#' By default, this service queries the models used by the latest GTEx release.
 #'
-#'[GTEx Portal API documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_collapsed_gene_model_exon_api_v2_dataset_collapsedGeneModelExon_get)
+#' [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_collapsed_gene_model_exon_api_v2_dataset_collapsedGeneModelExon_get)
 #'
-#'@inheritParams gtexr_arguments
+#' @inheritParams gtexr_arguments
 #'
-#'@return A Tibble
-#'@export
-#'@family Datasets Endpoints
+#' @return A tibble
+#' @export
+#' @family Datasets Endpoints
 #'
 #' @examples
-#' get_collapsed_gene_model_exon(gencodeIds = c("ENSG00000132693.12",
-#'                                                   "ENSG00000203782.5"))
-get_collapsed_gene_model_exon <- function(gencodeIds,
+#' \dontrun{
+#'  get_collapsed_gene_model_exon(gencodeId = "ENSG00000132693.12")
+#' }
+get_collapsed_gene_model_exon <- function(gencodeId,
                                           datasetId = "gtex_v8",
                                           page = 0,
                                           itemsPerPage = 250){
