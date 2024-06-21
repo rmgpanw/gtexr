c4e7479110d82419d42854ffbad5c141eb9aa080
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-21 22:14:08 +0100

    tweak documentation get_functional_annotation()

--- R/get_functional_annotation.R
+++ R/get_functional_annotation.R
@@ -1,15 +1,15 @@
-#'Get Functional Annotation
+#' Get Functional Annotation
 #'
-#'@description
-#' - This endpoint retrieves the functional annotation of a certain chromosome location. Default to most recent dataset release.
+#' @description This endpoint retrieves the functional annotation of a certain
+#' chromosome location. Default to most recent dataset release.
 #'
-#'[GTEx Portal API
-#'documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_full_get_collapsed_gene_model_exon_api_v2_dataset_fullCollapsedGeneModelExon_get)
+#' [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_full_get_collapsed_gene_model_exon_api_v2_dataset_fullCollapsedGeneModelExon_get)
 #'
-#'@inheritParams gtexr_arguments
-#'@return A Tibble
-#'@export
-#'@family Datasets Endpoints
+#' @inheritParams gtexr_arguments
+#' @return A tibble
+#' @export
+#' @family Datasets Endpoints
 #'
 #' @examples
 #' get_functional_annotation(chromosome = "chr1", start = 192168000, end = 192169000)
