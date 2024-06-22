da231f7c49cee4199f52a0f0d4ae894cdcf25f42
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-22 07:41:41 +0100

    update get_subject()

--- R/get_subject.R
+++ R/get_subject.R
@@ -1,20 +1,22 @@
-#'Get Subject
+#' Get Subject
 #'
-#'@description
-#' - This service returns information of subjects used in analyses from all datasets.
-#' - Results may be filtered by dataset ID, subject ID, sex, age bracket or Hardy Scale.
-#' - By default, this service queries the latest GTEx release.
+#' @description This service returns information of subjects used in analyses
+#' from all datasets. Results may be filtered by dataset ID, subject ID, sex,
+#' age bracket or Hardy Scale. By default, this service queries the latest GTEx
+#' release.
 #'
-#'[GTEx Portal API
-#'documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_tissue_site_detail_api_v2_dataset_tissueSiteDetail_get)
+#' [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_tissue_site_detail_api_v2_dataset_tissueSiteDetail_get)
 #'
-#'@inheritParams gtexr_arguments
-#'@return A Tibble
-#'@export
-#'@family Datasets Endpoints
+#' @inheritParams gtexr_arguments
+#' @return A Tibble
+#' @export
+#' @family Datasets Endpoints
 #'
 #' @examples
-#' get_subject()
+#' \dontrun{
+#'   get_subject()
+#' }
 get_subject <- function(datasetId = "gtex_v8",
                         sex = NULL,
                         ageBrackets = NULL,
