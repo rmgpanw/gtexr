bb68f6969e493dc4dedbcabdf9693bd3a610feee
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-22 09:08:22 +0100

    update get_linkage_disequilibrium_data()

--- R/get_linkage_disequilibrium_data.R
+++ R/get_linkage_disequilibrium_data.R
@@ -1,27 +1,32 @@
 #' Get Linkage Disequilibrium Data
 #'
-#' @description
-#' - Find linkage disequilibrium (LD) data for a given gene.
-#' - This endpoint returns linkage disequilibrium data for the cis-eQTLs found associated with the provided gene in a specified dataset.
-#' - Results are queried by gencode ID. By default, the service queries the latest GTEx release.
-#' - Specify a dataset ID to fetch results from a different dataset.
+#' @description Find linkage disequilibrium (LD) data for a given gene.
 #'
-#' [GTEx Portal API documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_variant_by_location_api_v2_dataset_variantByLocation_get)
+#' This endpoint returns linkage disequilibrium data for the cis-eQTLs found
+#' associated with the provided gene in a specified dataset. Results are queried
+#' by gencode ID. By default, the service queries the latest GTEx release.
+#' Specify a dataset ID to fetch results from a different dataset.
+#'
+#' [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Datasets-Endpoints/operation/get_variant_by_location_api_v2_dataset_variantByLocation_get)
 #'
 #' @inheritParams gtexr_arguments
 #' @return A tibble.
 #' @export
-#'@family Datasets Endpoints
+#' @family Datasets Endpoints
 #'
 #' @examples
-#' get_linkage_disequilibrium_data(gencodeIds = c("ENSG00000132693.12",
-#'                                                   "ENSG00000203782.5"))
-get_linkage_disequilibrium_data <- function(gencodeIds,
+#' get_linkage_disequilibrium_data(gencodeId = "ENSG00000132693.12")
+get_linkage_disequilibrium_data <- function(gencodeId,
                                             datasetId = "gtex_v8",
                                             page = 0,
                                             itemsPerPage = 250){
-  gtex_query(endpoint = "dataset/ld",
-             return_raw = TRUE)$data |>
+  result <- gtex_query(endpoint = "dataset/ld",
+             return_raw = TRUE)
+
+  paging_info_messages(result)
+
+  result$data |>
     purrr::map(\(x) tibble::tibble(variants = x[[1]], ld = x[[2]])) |>
     dplyr::bind_rows() |>
     tidyr::separate_wider_delim(
