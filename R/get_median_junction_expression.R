3cffb794230eac6e60ffd2d18186b0d1710a0fb2
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-23 20:47:53 +0100

    update get_median_junction_expression()

--- R/get_median_junction_expression.R
+++ R/get_median_junction_expression.R
@@ -1,17 +1,18 @@
 #' Get Median Junction Expression
 #'
-#' @description
-#' Find junction gene expression data.
-#' Returns median junction read counts in tissues of a given gene from all known transcripts.
-#' Results may be filtered by dataset or tissue.
+#' @description Find junction gene expression data.
+#'
+#' - Returns median junction read counts in tissues of a given gene from all known transcripts.
+#' - Results may be filtered by dataset or tissue.
+#'
 #' By default, this service queries the latest GTEx release.
 #'
 #' [GTEx Portal API
-#'documentation](https://gtexportal.org/api/v2/redoc#tag/Expression-Data-Endpoints/operation/get_median_junction_expression_api_v2_expression_medianJunctionExpression_get)
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Expression-Data-Endpoints/operation/get_median_junction_expression_api_v2_expression_medianJunctionExpression_get)
 #'
-#'@inheritParams gtexr_arguments
+#' @inheritParams gtexr_arguments
 #'
-#' @return A tibble
+#' @return A tibble.
 #' @export
 #' @family Expression Data Endpoints
 #'
