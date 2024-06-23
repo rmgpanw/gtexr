cfa0a38b28ca4a82ec14d96522e14aeb8597159d
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-23 09:33:13 +0100

    update get_median_exon_expression()

--- R/get_median_exon_expression.R
+++ R/get_median_exon_expression.R
@@ -1,23 +1,26 @@
 #' Get Median Exon Expression
 #'
-#' @description
-#' - Find median exon expression data.
+#' @description Find median exon expression data.
+#'
 #' - Returns median exon read counts, in tissues, of a collapsed gene model.
 #' - Results may be filtered by dataset, gene or tissue, but at least one gene must be provided
-#' - By default, this service queries the latest GTEx release.
 #'
-#'[GTEx Portal API
-#'documentation](https://gtexportal.org/api/v2/redoc#tag/Expression-Data-Endpoints/operation/get_median_exon_expression_api_v2_expression_medianExonExpression_get)
+#' By default, this service queries the latest GTEx release.
+#'
+#' [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Expression-Data-Endpoints/operation/get_median_exon_expression_api_v2_expression_medianExonExpression_get)
 #'
 #' @inheritParams gtexr_arguments
 #'
-#' @return A tibble
+#' @return A tibble.
 #' @export
 #' @family Expression Data Endpoints
 #'
 #' @examples
 #' \dontrun{
-#' get_median_exon_expression(gencodeIds = "ENSG00000132693.12")
+#' # median exon expression values for CRP, filtered for whole blood
+#' get_median_exon_expression(gencodeIds = "ENSG00000132693.12",
+#'                            tissueSiteDetailIds = "Whole_Blood")
 #' }
 get_median_exon_expression <- function(gencodeIds,
                                        datasetId = "gtex_v8",
