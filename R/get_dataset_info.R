7b6037191cd44e02c3a0f8f4727923401ee104c5
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-24 22:41:41 +0100

    update get_image() and get_dataset_info()

--- R/get_dataset_info.R
+++ R/get_dataset_info.R
@@ -1,6 +1,9 @@
 #' Get Dataset Info
 #'
-#' NB This current returns empty values.
+#' @description [GTEx Portal API
+#'   documentation](https://gtexportal.org/api/v2/redoc#tag/Metadata-Endpoints/operation/get_dataset_info_api_v2_metadata_dataset_get)
+#'
+#' @details Note: this current only returns empty values.
 #'
 #' @inheritParams gtexr_arguments
 #'
@@ -9,7 +12,9 @@
 #' @family Metadata Endpoints
 #'
 #' @examples
+#' \dontrun{
 #' get_dataset_info()
+#' }
 get_dataset_info <- function(datasetId = "gtex_v8",
                       organizationName = "GTEx Consortium") {
   gtex_query(endpoint = "metadata/dataset")
--- R/get_image.R
+++ R/get_image.R
@@ -1,5 +1,8 @@
 #' Get Image
 #'
+#' @description [GTEx Portal API
+#' documentation](https://gtexportal.org/api/v2/redoc#tag/Histology-Endpoints/operation/get_image_api_v2_histology_image_get)
+#'
 #' @inheritParams gtexr_arguments
 #'
 #' @return A tibble.
@@ -7,8 +10,12 @@
 #' @family Histology Endpoints
 #'
 #' @examples
+#' \dontrun{
 #' get_image()
+#'
+#' # filter by `tissueSampleId`
 #' get_image(tissueSampleIds = "GTEX-1117F-0226", page = 0, itemsPerPage = 100000)
+#' }
 get_image <- function(tissueSampleIds = NULL,
                       page = 0,
                       itemsPerPage = 250) {
